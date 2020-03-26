504d96c6c48e66efe4c3707ea0794d9e4f2c5076
Miroslaw Puczynski <miroslaw@puczynski.org>
2020-03-26 16:31:16 +0100

    [WIP] implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+
+struct Student {
+    name: String
+}
+
+impl Student {
+    fn new(name: &str) -> Student
+    {
+        Student {
+            name: name.into()
+        }
+    }
+}
+
+struct Course {
+    name: String
+}
+
+
+struct Enrollment<'a> 
+{
+    student: &'a Student,
+    course: &'a Course
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }
